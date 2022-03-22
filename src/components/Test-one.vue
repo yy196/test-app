3f6f72aa6c2cce5cd71d2648b4afc44125443179
wanggaofei <wanggaofei@pacvue.cn>
2022-03-22 15:54:16 +0800

    y2

--- src/components/Test-one.vue
+++ src/components/Test-one.vue
@@ -1,23 +1,7 @@
 <template>
   <div>
     <span>6</span>
-    <span>0</span>
-    <span>888888</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>6666</span>
-    <span>6666</span>
-    <span>6666</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
+    <div>######</div>
     
 
   </div>
