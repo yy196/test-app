1a2e684b2e21d7703a811787a1edae55acc4c806
yy196 <75899637+yy196@users.noreply.github.com>
2022-03-22 12:07:48 +0800

    reset

--- src/components/Test-one.vue
+++ src/components/Test-one.vue
@@ -1,17 +1,6 @@
 <template>
   <div>
     <span>111111</span>
-    <span>222222</span>
-    <span>9999999</span>
-    <span>888888</span>
-    <span>9999999</span>
-    <span>7777777</span>
-    <span>55555</span>
-    <span>******</span>
-    <span>#######</span>
-    <span&&&&&&&&span>
-    <span----------span>
-  </div>
 </template>
 
 <script>
