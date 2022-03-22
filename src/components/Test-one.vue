7ea3063eab009398465c597b49217cb3703b8189
wanggaofei <wanggaofei@pacvue.cn>
2022-03-22 15:36:38 +0800

    hhhhh

--- src/components/Test-one.vue
+++ src/components/Test-one.vue
@@ -3,6 +3,15 @@
     <span>6</span>
     <span>0</span>
     <span>888888</span>
+    <span>0</span>
+    <span>0</span>
+    <span>0</span>
+    <span>0</span>
+    <span>0</span>
+    <span>9999</span>
+    <span>9999</span>
+    <span>9999</span>
+    <span>9999</span>
     
 
   </div>
