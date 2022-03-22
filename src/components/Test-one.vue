31a2b92b2b07956454e03070618f13357527ea4c
yy196 <75899637+yy196@users.noreply.github.com>
2022-03-22 11:36:49 +0800

    delete and add 9999

--- src/components/Test-one.vue
+++ src/components/Test-one.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <span>1111</span>
-    <span>2222</span>
-    <span>33333</span>
-    <span>55555</span>
-    <span>44444</span>
     <span>0000000</span>
+<span>9999999</span>
   </div>
 </template>
 
