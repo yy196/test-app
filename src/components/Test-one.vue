21ead945997b8a34939f387e89e417e876060be6
yy196 <75899637+yy196@users.noreply.github.com>
2022-03-22 15:34:58 +0800

    add 888

--- src/components/Test-one.vue
+++ src/components/Test-one.vue
@@ -2,12 +2,7 @@
   <div>
     <span>6</span>
     <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
+    <span>888888</span>
     
 
   </div>
