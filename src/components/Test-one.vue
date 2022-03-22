879760f0d8d14922b210dda6c39c4ba42fe008ca
yy196 <75899637+yy196@users.noreply.github.com>
2022-03-22 15:53:43 +0800

    y

--- src/components/Test-one.vue
+++ src/components/Test-one.vue
@@ -1,23 +1,7 @@
 <template>
   <div>
-    <span>6</span>
-    <span>0</span>
-    <span>888888</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>0</span>
-    <span>6666</span>
-    <span>6666</span>
-    <span>6666</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
-    <span>88888</span>
+    <i>$$$$$</i>
+    <span>44444</span>
     
 
   </div>
